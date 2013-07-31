9da2e24b252c9c507766589316c800875d1d5d5c
Mooneer Salem <mooneer@gmail.com>
2013-07-31 00:36:18 -0700

    completed insert code for background task

--- src/main/scala/com/thoughtbug/newsrdr/tasks/RssFetchJob.scala
+++ src/main/scala/com/thoughtbug/newsrdr/tasks/RssFetchJob.scala
@@ -14,7 +14,6 @@ class RssFetchJob extends Job {
     val feed = new RSSFeed
     feed.load(feedUrl)
     
-    // TODO: insert into db
     BackgroundJobManager.db withSession {
       // Update feed's contents with whatever we've fetched from the server.
       val feedQuery = Query(NewsFeeds)
@@ -59,6 +58,80 @@ class RssFetchJob extends Job {
       for { c <- categoryIds } {
         (NewsFeedCategories.feedId ~ NewsFeedCategories.categoryId).insert(c)
       }
+      
+      // Now update/insert each individual post in the feed.
+      for { p <- feed.entries } insertOrUpdateEntry(newsFeedId, p)
     } 
+  }
+  
+  private def insertOrUpdateEntry(feedId: Int, p: (NewsFeedArticle, List[String])) {
+    val newPost = p._1
+    
+    // Insert or update article as needed.
+    val existingEntryId = for { 
+      e <- NewsFeedArticles if e.guid === newPost.guid || 
+                              (e.title === newPost.title && e.link === newPost.link && e.description === newPost.description)
+      } yield e.id
+    val entry = for { 
+      e <- NewsFeedArticles if e.guid === newPost.guid || 
+                              (e.title === newPost.title && e.link === newPost.link && e.description === newPost.description)
+      } yield (
+          e.title ~ e.link ~ e.description ~ e.author ~ e.comments ~ e.enclosureLength ~ 
+          e.enclosureType ~ e.enclosureUrl ~ e.guid ~ e.isGuidPermalink ~ e.pubDate ~
+          e.source)
+    val entryId = entry.firstOption match {
+        case Some(ent) => {
+          entry.update(
+            newPost.title,
+            newPost.link,
+            newPost.description,
+            newPost.author,
+            newPost.comments,
+            newPost.enclosureLength,
+            newPost.enclosureType,
+            newPost.enclosureUrl,
+            newPost.guid,
+            newPost.isGuidPermalink,
+            newPost.pubDate,
+            newPost.source)
+          existingEntryId.first
+        }
+        case None => (
+            NewsFeedArticles.feedId ~ NewsFeedArticles.title ~ NewsFeedArticles.link ~ NewsFeedArticles.description ~
+            NewsFeedArticles.author ~ NewsFeedArticles.comments ~ NewsFeedArticles.enclosureLength ~
+            NewsFeedArticles.enclosureType ~ NewsFeedArticles.enclosureUrl ~ NewsFeedArticles.guid ~
+            NewsFeedArticles.isGuidPermalink ~ NewsFeedArticles.pubDate ~ NewsFeedArticles.source) returning NewsFeedArticles.id insert(
+                feedId,
+                newPost.title,
+	            newPost.link,
+	            newPost.description,
+	            newPost.author,
+	            newPost.comments,
+	            newPost.enclosureLength,
+	            newPost.enclosureType,
+	            newPost.enclosureUrl,
+	            newPost.guid,
+	            newPost.isGuidPermalink,
+	            newPost.pubDate,
+	            newPost.source
+            )
+    }
+      
+    // Update feed categories.
+    val categoryIds = p._2.map((c) => {
+	    val feedQuery = for { fc <- Categories if fc.name === c } yield fc
+	    feedQuery.firstOption match {
+	      case Some(cat) => (entryId, cat.id.get)
+	      case None => { 
+	        val newId = (Categories.name) returning Categories.id insert(c)
+	        (entryId, newId)
+	      }
+	    }
+      })
+      val postCategories = for { nfc <- NewsFeedArticleCategories if nfc.articleId === entryId } yield nfc
+      postCategories.delete
+      for { c <- categoryIds } {
+        (NewsFeedArticleCategories.articleId ~ NewsFeedArticleCategories.categoryId).insert(c)
+      }
   }
 }
