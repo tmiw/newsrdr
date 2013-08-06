68fbfb14b544a5cb02deed1120de67dc170525f0
Mooneer Salem <mooneer@gmail.com>
2013-08-06 00:44:15 -0700

    Space out jobs by starting 60 minutes from the current date/time instead of the next hour.

--- src/main/scala/com/thoughtbug/newsrdr/tasks/BackgroundJobManager.scala
+++ src/main/scala/com/thoughtbug/newsrdr/tasks/BackgroundJobManager.scala
@@ -1,15 +1,14 @@
 package com.thoughtbug.newsrdr.tasks
 
 import com.thoughtbug.newsrdr.models.DataTables
-
 import scala.slick.session.Database
 import org.quartz.impl.StdSchedulerFactory
-
 import org.quartz.JobBuilder._
 import org.quartz.TriggerBuilder._
 import org.quartz.SimpleScheduleBuilder._
 import org.quartz.DateBuilder._
 import org.quartz._
+import java.util.DateTime
 
 object BackgroundJobManager {
   var db : Database = _
@@ -28,7 +27,7 @@ object BackgroundJobManager {
     if (!scheduler.checkExists(new JobKey(url))) {
       val trigger = newTrigger()
     		.withIdentity(url)
-    		.startAt(evenHourDate(null))
+    		.startAt(futureDate(60, IntervalUnit.MINUTE))
     		.startNow()
     		.withSchedule(simpleSchedule().withIntervalInHours(1).repeatForever())
     		.build()
