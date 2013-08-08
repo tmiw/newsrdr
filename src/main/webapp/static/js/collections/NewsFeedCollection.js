c17373c41fa46298630ebdd895d15e634b7f8fdb
Mooneer Salem <mooneer@gmail.com>
2013-08-08 00:21:48 -0700

    a better way of sorting strings

--- src/main/webapp/static/js/collections/NewsFeedCollection.js
+++ src/main/webapp/static/js/collections/NewsFeedCollection.js
@@ -8,16 +8,11 @@ var NewsFeedCollection = Backbone.Collection.extend({
 	comparator: function(obj1, obj2) {
 		var diff = -(obj1.get("numUnread") - obj2.get("numUnread"));
 		if (diff == 0) {
-			var title1 = obj1.get("feed").title;
-			var title2 = obj2.get("feed").title;
-			for (var i = 0; i < (title1.length < title2.length ? title1.length : title2.lengt); i++)
-			{
-				diff = title1.charCodeAt(i) - title2.charCodeAt(i);
-				if (diff != 0) break;
-			}
-			if (diff == 0) {
-				diff = title1.length - title2.length;
-			}
+			var title1 = obj1.get("feed").title.toLowerCase();
+			var title2 = obj2.get("feed").title.toLowerCase();
+			if (title1 < title2) diff = -1;
+			else if (title1 > title2) diff = 1;
+			else diff = 0;
 		} 
 		return diff;
 	},
