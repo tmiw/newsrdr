08d65ce2f1c2f5b4611381148adbcfc073c56d8b
Mooneer Salem <mooneer@gmail.com>
2013-08-16 21:02:18 -0700

    attempt to break deadlock

--- src/main/scala/com/thoughtbug/newsrdr/tasks/RssFetchJob.scala
+++ src/main/scala/com/thoughtbug/newsrdr/tasks/RssFetchJob.scala
@@ -14,10 +14,30 @@ class RssFetchJob extends Job {
   def fetch(feedUrl: String): NewsFeed = {    
     val feed = XmlFeedFactory.load(feedUrl)
     
-    BackgroundJobManager.db withTransaction { implicit session: Session =>
-      // Update feed's contents with whatever we've fetched from the server.
-      // If it doesn't already exist, create.
-      BackgroundJobManager.dao.updateOrInsertFeed(session, feedUrl, feed)
-    } 
+    // Allow up to three times to perform transaction.
+    var count = 0
+    while (count < 3)
+    {
+      try {
+        return BackgroundJobManager.db withTransaction { implicit session: Session =>
+          // Update feed's contents with whatever we've fetched from the server.
+          // If it doesn't already exist, create.
+          BackgroundJobManager.dao.updateOrInsertFeed(session, feedUrl, feed)
+        }
+      } catch {
+        case e:java.sql.SQLException => {
+          if (e.toString().contains("Lock wait timeout exceeded"))
+          {
+            count = count + 1
+          }
+          else
+          {
+            throw e
+          }
+        }
+      }
+    }
+    
+    throw new RuntimeException("Could not break deadlock.")
   }
 }
