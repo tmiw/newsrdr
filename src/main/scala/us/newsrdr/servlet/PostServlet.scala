185683bdd0b07754c35450997e40668da3f00d0a
Mooneer Salem <mooneer@gmail.com>
2013-12-21 02:37:51 -0800

    Maybe not.

--- src/main/scala/us/newsrdr/servlet/PostServlet.scala
+++ src/main/scala/us/newsrdr/servlet/PostServlet.scala
@@ -68,8 +68,8 @@ class PostServlet(dao: DataTables, db: Database, implicit val swagger: Swagger) 
         }
         
         val latestPostId = params.get("latest_post_id") match {
-            case Some(x) if !x.isEmpty() => java.lang.Long.parseLong(x)
-            case _ => dao.getMaxPostIdForAllFeeds(userId, unreadOnly, latestPostDate)
+            //case Some(x) if !x.isEmpty() => java.lang.Long.parseLong(x)
+            case _ => Long.MaxValue // dao.getMaxPostIdForAllFeeds(userId, unreadOnly, latestPostDate)
         }
         
         ArticleListWithMaxId(
