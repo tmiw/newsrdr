84a12d3c9fe8b48f1ff26493c62ed5a4089ff4f4
Mooneer Salem <mooneer@gmail.com>
2013-12-20 21:50:21 -0800

    Update Slick ORM library.

--- project/build.scala
+++ project/build.scala
@@ -38,7 +38,7 @@ object NewsrdrBuild extends Build {
         "org.ccil.cowan.tagsoup" % "tagsoup" % "1.2",
         "com.github.nscala-time" %% "nscala-time" % "0.4.2",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "com.typesafe.slick" %% "slick" % "1.0.0",
+        "com.typesafe.slick" %% "slick" % "1.0.1",
         "org.slf4j" % "slf4j-nop" % "1.6.4",
         "com.h2database" % "h2" % "1.3.166",
         "mysql" % "mysql-connector-java" % "5.1.26",
