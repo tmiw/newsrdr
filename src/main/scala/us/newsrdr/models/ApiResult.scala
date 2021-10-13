bd9450b4ec9de3299a29c44c583bd4c57a038c9b
Mooneer Salem <mooneer@gmail.com>
2021-10-13 00:41:58 -0700

    Revert URL changes; we always use https on production.

--- src/main/scala/us/newsrdr/models/ApiResult.scala
+++ src/main/scala/us/newsrdr/models/ApiResult.scala
@@ -12,9 +12,7 @@ object Constants {
   }
   
   def getAuthenticatedURL(request: javax.servlet.http.HttpServletRequest, service: String) : String = {
-      val portSegment = if (request.getServerPort() != 80 && request.getServerPort() != 443) (":" + request.getServerPort().toString()) else ""
-      val protocol = if (request.isSecure()) "https" else "http"
-    (protocol + "://" + request.getServerName() + portSegment +  Constants.AUTHENTICATED_URI + "/" + service)
+      (getURL(request, Constants.AUTHENTICATED_URI + "/" + service))
   }
   
   def getGoogleLoginURL(request: javax.servlet.http.HttpServletRequest) : String = {
